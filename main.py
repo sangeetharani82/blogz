2b06406f548dc4b6265ef21e49ef2dd956ad7cd7
Sangeetha Rani <sangeetha.sabarinath@gmail.com>
2018-10-16 11:34:21 -0500

    user-class, login, signup and both validation works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:Amba26aug1956!@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -67,7 +68,7 @@ def main_blog():
         return render_template('blog.html', blog_titles=blog_titles, page_title='Blogz', 
             blog_bodyz=blog_bodyz)
 
-# ---------------------User------------
+# ---------------------User/logn/signup------------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
@@ -80,11 +81,58 @@ class User(db.Model):
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == '' or password == '':#empty fields
+            flash('Invalid username', 'error')
+            return redirect('/login')
+        user = User.query.filter_by(username=username).first()
+        if user and user.password != password:#pswd incorrect
+            flash('Password is incorrect', 'error')
+            return redirect('/login')
+        if user and user.password == password: #validation succeeds
+            session['username'] = username
+            flash("Logged in", 'information')
+            flash('Welcome back ' + username.capitalize() + '!', 'information')
+            #print(session)
+            return redirect('/newpost')
+        elif not user:#new user
+            flash('Username does not exist', 'error')
+            return redirect('/login')
+
     return render_template('login.html')
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        if username == '' or password == '' or verify == '':#empty fields
+            flash('One or more fields are invalid', 'error')
+            return redirect('/signup')
+        if len(username) < 3:
+            flash('Invalid username', 'error')
+            return redirect('/signup')
+        if len(password) < 3 or len(verify) < 3:
+            flash('Invalid password', 'error')
+            return redirect('/signup')
+        else:
+            if password != verify:#mismatch pswd
+                flash("Password don't match", 'error')
+                return redirect('/signup')
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:#new user
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Welcome to your blog, ' + username.capitalize() + '!', 'information')
+            return redirect('/newpost')
+        else:
+            flash('Username already exists', 'error')#same username used to create one more account
+            return redirect('/signup')
+
     return render_template('signup.html')
 
 @app.route("/")
